4df2f1642c6b947496bdbf2431658418202a98b6
Carlos Escalona <carlos.armando.escalona@gmail.com>
2023-11-04 21:17:45 +1100

    Adding a different way to show results - FeatureZ

--- main.py
+++ main.py
@@ -3,13 +3,21 @@
 
 # This is a simple code in python of a program that count lines in a file called text.txt.
 
-# Simple function to count lines
+# Function to count lines
 def count_lines(file):
+    file.seek(0)  # Reset file pointer to the start
     return sum(1 for line in file)
 
-# Main execution
+# Function to count words
+def count_words(file):
+    file.seek(0)  # Reset file pointer to the start
+    return sum(len(line.split()) for line in file)
+
+# Main execution for feature-z
 filename = 'text.txt'
 with open(filename, 'r') as file:
     line_count = count_lines(file)
+    word_count = count_words(file)
 
-print(f"The number of lines in the file is: {line_count}")
+# Modified print statement for feature-z
+print(f"[Feature Z] The file '{filename}' has {line_count} lines and {word_count} words.")
